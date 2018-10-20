691fcc2944b5ea42dfdd2ab8e2e1aa3fbfc15c18
David Costa <davidcostadev@gmail.com>
2018-10-20 08:25:42 -0300

    wip

--- src/pages/Reports.vue
+++ src/pages/Reports.vue
@@ -1,5 +1,50 @@
 <template>
   <div>
-    report
+    <md-toolbar>
+      <h3 class="md-title">Relátorio</h3>
+    </md-toolbar>
+    <md-content>
+      <div id="graphs">
+        <apexcharts width="500" type="pie" :options="chart" :series="series"></apexcharts>
+      </div>
+    </md-content>
   </div>
 </template>
+
+<script>
+import VueApexCharts from 'vue-apexcharts'
+
+export default {
+  components: {
+    apexcharts: VueApexCharts,
+  },
+  data: function() {
+    return {
+      chart: {
+        labels: ["Apple", "Mango", "Banana", "Papaya", "Orange"],
+        theme: {
+          palette: 'palette5' // upto palette10
+        },
+        dataLabels: {
+          style: {
+            // colors: ['#F44336', '#E91E63', '#9C27B0']
+          }
+        },
+        markers: {
+          colors: ['#F44336', '#E91E63', '#9C27B0']
+        }
+      },
+      series: [44, 55, 13, 43, 22]
+    }
+  },
+};
+</script>
+
+<style>
+  #graphs {
+    display: flex;
+    justify-content: center;
+    color: white;
+  }
+</style>
+
